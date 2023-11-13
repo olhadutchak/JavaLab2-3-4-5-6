2ca5b03ea4841a7c9b9e8e93b7bd8a9d933f49e0
olhadutchak <135028848+olhadutchak@users.noreply.github.com>
2023-11-13 23:16:08 +0200

    Delete lab2 directory

--- lab2/Class2lab/app/src/main/java/class2lab/Serializable.java
+++
@@ -1,8 +0,0 @@
-package class2lab;
-
-import java.io.IOException;
-
-public interface Serializable<T> {
-    void serialize(String fileName, T object) throws IOException;
-    T deserialize(String fileName, Class<T> clazz) throws IOException, ClassNotFoundException;
-}
